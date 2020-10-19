a66ef8d5fb89f601b577c497558840890e7a0469
Adrian Skilling <adrian.skilling@gmail.com>
2020-10-19 17:07:47 +0100

    Use debug

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ KSP=~/KSP_Cutdown
 BGDIR=$KSP/GameData/BoosterGuidance
 
 mkdir -p $BGDIR
-cp ./obj/Release/BoosterGuidance.dll $BGDIR
+cp ./obj/Debug/BoosterGuidance.dll $BGDIR
 cp BoosterGuidance.cfg $BGDIR
 cp BoosterGuidance.version $BGDIR]
 cp *.png $BGDIR
