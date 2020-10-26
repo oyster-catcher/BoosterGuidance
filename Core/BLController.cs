60b243a56a85588071fed4054a980c9ee9025925
Adrian Skilling <adrian.skilling@gmail.com>
2020-10-26 16:35:42 +0000

    Add aero dynamic descent settings

--- Core/BLController.cs
+++ Core/BLController.cs
@@ -24,6 +24,8 @@ namespace BoosterGuidance
     public double poweredDescentAlt = 5000; // Powered descent below this altitude
     public double aeroDescentAlt = 70000; // Aerodynamic descent below this altitude
     private double minError = float.MaxValue;
+    public double steerGain = 0.05;
+    public double maxAoA = 10;
     public Vector3d predWorldPos = Vector3d.zero;
     public bool shutdown = false; // shutdown engines once
     public BLControllerPhase phase = BLControllerPhase.BoostBack;
@@ -58,6 +60,7 @@ namespace BoosterGuidance
 
     public void SetPhase(BLControllerPhase a_phase)
     {
+      minError = float.MaxValue; // reset so boostback doesn't give up
       phase = a_phase;
     }
 
@@ -79,6 +82,8 @@ namespace BoosterGuidance
       double throttleGain = 5.0/(amax-amin); // correct 1 m/s error in 0.2 second
       targetError = 0;
 
+      Vector3d att = new Vector3d(vessel.transform.up.x, vessel.transform.up.y, vessel.transform.up.z);
+
       // No thrust - retrograde (default and Coasting phase)
       throttle = 0;
       steer = -Vector3d.Normalize(v);
@@ -90,32 +95,53 @@ namespace BoosterGuidance
       tc.aeroDescentAlt = poweredDescentAlt; // No aero descent
       tc.phase = BLControllerPhase.Coasting;
       predWorldPos = Simulate.ToGround(tgtAlt, vessel, body, tc, 2, out targetT);
-      Vector3d error = tgt_r - predWorldPos;
+      Vector3d error = predWorldPos - tgt_r;
       targetError = error.magnitude;
+      attitudeError = 0;
 
       // BOOSTBACK
       if (phase == BLControllerPhase.BoostBack)
       {
-        double ba = 0.003 * error.magnitude;
-        // TODO: Consider time to impact
+        // Aim to close max of 20% of error in 1 second
+        steer = -Vector3d.Normalize(error);
+        
+        attitudeError = Math.Acos(Vector3d.Dot(att, steer)) * 180/Math.PI;
+        Debug.Log("Attitude error=" + attitudeError);
+        double ba = 0;
+        if (attitudeError < 90)
+          ba = Math.Max(10,error.magnitude) / Math.Min(targetT,10);
         throttle = Mathf.Clamp((float)((ba - amin) / (amax - amin)), 0, 1);
         // Stop if error has grown significantly
-        if (steer.magnitude > minError * 1.2)
+        if ((targetError > minError * 1.2) || (targetError < 10))
         {
           phase = BLControllerPhase.Coasting;
         }
-        steer = Vector3d.Normalize(steer);
+        minError = Math.Min(targetError, minError);
+        // TODO: Add in elevation
         if (y < aeroDescentAlt)
           phase = BLControllerPhase.AeroDescent;
       }
 
       // COASTING
-      if (y < aeroDescentAlt)
-        phase = BLControllerPhase.AeroDescent;
+      if (phase == BLControllerPhase.Coasting)
+      {
+        if (y < aeroDescentAlt)
+          phase = BLControllerPhase.AeroDescent;
+      }
 
       // AERO DESCENT
       if (phase == BLControllerPhase.AeroDescent)
       {
+        // Aero dynamic descent
+        Vector3d adj = error * steerGain * 0.01;
+        // good approx for small angles
+        double maxAdj = Math.Sin(maxAoA * Mathf.PI / 180);
+        if (adj.magnitude > maxAdj)
+          adj = Vector3d.Normalize(adj) * maxAdj;
+        steer = -Vector3d.Normalize(vessel.GetSrfVelocity()) + adj;
+        float ddot = (float)Vector3d.Dot(Vector3d.Normalize(att), Vector3d.Normalize(steer));
+        double att_err = Mathf.Acos(ddot) * 180 / Mathf.PI;
+
         if (y < poweredDescentAlt)
           phase = BLControllerPhase.PoweredDescent;
       }
